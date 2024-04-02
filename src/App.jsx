83d7746dbe9ec8117414c8f431c07e8710373983
itforget CORDEIRO <itforget11@gmail.com>
2024-04-02 16:40:54 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div className='bg-[#5A5A5A] flex flex-row w-auto h-32 items-center gap-3'>
+        <img className=' h-20 w-20 ml-3' src='/icone.png'></img>
+          <h1 className='bg-transparent flex flex-col font-bold text-2xl text-[#8FC73B] border-l border-slate-200 p-2'>Filhas
+            <span className='bg-transparent text-[#713910]'>da terra</span>
+          </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
